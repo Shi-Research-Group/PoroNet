9573387a268c58ba7d4260f54c903261a18d5779
czheng37 <152939276+czheng37@users.noreply.github.com>
2025-08-26 15:54:50 -0400

    Update __init__.py

--- poronet_functions/__init__.py
+++ poronet_functions/__init__.py
@@ -1,18 +1,5 @@
-"""A powerful python package for the analysis, visualization, featurization and machine learning on molecular pores in nanoporous materials."""
-
-# Add imports here
 from .compute import *
-#from .analyze import *
 from .visualize import *
 from .helper import *
 from .data import *
-#from .workflow import *
 from .raspa_helper import *
-
-
-# Handle versioneer
-#from ._version import get_versions
-#versions = get_versions()
-#__version__ = versions['version']
-#__git_revision__ = versions['full-revisionid']
-#del get_versions, versions
